928ffcf228380ba86a602715a0b4602b9c7be7b6
Jinesh <jineshkb2@gmail.com>
2014-09-16 21:09:02 +0530

    Update cachematrix.R
    Adding comment for particular execution and function.

--- cachematrix.R
+++ cachematrix.R
@@ -4,11 +4,11 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
-    set <- function(y=matrix()) {
-        x <<- y
+    m <- NULL  # sets the value of m to NULL (provides a default if cacheSolve has not yet been used)
+    set <- function(y=matrix()) {    #set the value of the matrix
+        x <<- y            ## caches the inputted matrix so that cacheSolve can check whether it has changed (note this is within the setmatrix function)
         
-        m <<- NULL
+        m <<- NULL      # # sets the value of m (the matrix inverse if used cacheSolve) to NULL
     }
     get <- function() x
     setinverse <- function(solve) m <<- solve
@@ -29,11 +29,30 @@ cacheSolve <- function(x, ...) {
         message("getting cached data")
         return(m)
     }
-    y <- x$get()
-    
-    x$set(y)
+    y <- x$get()      # run the getmatrix function to get the value of the input matrix
+    x$set(y)          # run the setmatrix function on the input matrix to cache it
     inverse <- x$get()
-    m <- solve(y, ...)
-    x$setinverse(m)
-    m
+    m <- solve(y, ...)  # compute the value of the inverse of the input matrix
+    x$setinverse(m)     # run the setinverse function on the inverse to cache the inverse
+    m                   # return the inverse
 }
+## Sample run:
+
+
+##Store matrix in d 
+##  d<-makeCacheMatrix(matrix(1:4,2,2))
+##  No cache while running for the first time.
+##  1st time computing
+##> cacheSolve(d)
+##      [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
+
+##Computing again
+
+##2nd Time
+##cacheSolve(d)
+##getting cached data
+##      [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
